81c0bdcb3eb51370b3d338bed0bb57285c00a2f7
jhoon9494 <wjdgns9406@gmail.com>
2022-02-16 16:28:03 +0900

    custom Hook with useReducer

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,24 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return { ...state, [action.name]: action.value };
+    case "RESET_INPUT":
+      return state;
+    default:
+      return state;
+  }
+};
 
 export const useInput = (initialValue) => {
-  const [inputs, setInputs] = useState(initialValue);
+  const [state, dispatch] = useReducer(reducer, initialValue);
   const onChange = useCallback((e) => {
     const { value, name } = e.target;
-    setInputs((prevInput) => {
-      return { ...prevInput, [name]: value };
-    });
+    dispatch({ type: "CHANGE_INPUT", value, name });
   }, []);
-  const reset = useCallback(() => setInputs(initialValue), [initialValue]);
-  return [inputs, onChange, reset];
+  const reset = useCallback(() => {
+    dispatch({ type: "RESET_INPUT" });
+  }, []);
+  return [state, onChange, reset];
 };
